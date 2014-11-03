e251654a1edd953edc82e494633a3ce8a99f1f3a
Andreas M. Brændhaugen <andreas@lever.co>
2014-11-02 20:02:36 -0800

    ndxz-studio

---
+++ v1/ndxz-studio/config/config.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'sarah_portfolio';
+$indx['user'] 		= 'srhnhm';
+$indx['pass'] 		= 'sdflkj11';
+$indx['host'] 		= 'database.ambwork.com';
+$indx['sql']		= 'mysql';
+
+?>
